63cd2a3f235419952c688fd5c719f4c1d232ecae
ruvilonix <a@a.a>
2025-06-29 12:49:23 -0400

    started view for adding molecules

--- sickgenes/views.py
+++ sickgenes/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from sickgenes.forms import MoleculeMatchForm
 
 def add_molecules(request):
-    form = MoleculeMatchForm()
+    if request.method == 'GET':
+        form = MoleculeMatchForm()
+    else:
+        form = MoleculeMatchForm(request.POST)
+        if form.is_valid():
+            pass
+                                 
     return render(request, 'sickgenes/molecule_match.html', {'form': form})
