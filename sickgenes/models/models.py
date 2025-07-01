d0b48c23883d10d60afef049abf09781859190c4
ruvilonix <a@a.a>
2025-07-01 15:52:14 -0400

    removed old molecule models

--- sickgenes/models/models.py
+++ sickgenes/models/models.py
@@ -43,76 +43,12 @@ class Finding(models.Model):
         ABUNDANCE = "A", _("Molecular abundance")
 
     study_cohort = models.ForeignKey(StudyCohort, on_delete=models.CASCADE)
-    molecule = models.ForeignKey('Molecule', on_delete=models.PROTECT)
+    hgnc_gene = models.ForeignKey('HgncGene', on_delete=models.PROTECT, null=True, default=None)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['study_cohort', 'molecule'], name='unique_study_gene'),
+            models.UniqueConstraint(fields=['study_cohort', 'hgnc_gene'], name='unique_study_gene'),
         ]
 
     def __str__(self):
         return f"[{self.study_cohort.study.title[:20]}]... - {self.molecule}"
-
-class Molecule(models.Model):
-    class MoleculeType(models.TextChoices):
-        GENE = "G", _("Gene")
-        METABOLITE = "M", _("Metabolite")
-
-    hgnc_id = models.CharField(max_length=40, default=None, unique=True, null=True)
-    hgnc_symbol = models.CharField(max_length=50, default=None, unique=True, null=True)
-    hgnc_name = models.CharField(max_length=250, default=None, null=True)
-
-    hmdb_accession = models.CharField(max_length=100, default=None, unique=True, null=True)
-    hmdb_name = models.CharField(max_length=400, default=None, unique=True, null=True)
-
-    type = models.CharField(max_length=1, choices=MoleculeType)
-
-    datetime_updated = models.DateTimeField(null=True)
-
-    objects = MoleculeManager()
-
-    class Meta:
-        verbose_name = 'molecule'
-        verbose_name_plural = 'molecules'
-
-        ordering = ['hgnc_symbol', 'hmdb_name']
-
-        indexes = [
-            models.Index(fields=['type'], name='molecule_type'),
-            models.Index(fields=['hgnc_name'], name='hgnc_name_index')
-        ]
-
-
-    def __str__(self):
-        if self.type == self.MoleculeType.GENE:
-            return self.hgnc_symbol
-        elif self.type == self.MoleculeType.METABOLITE:
-            return self.hmdb_name
-        else:
-            return '<NO NAME>'
-        
-    # TODO add check constraint where if type is GENE, hgnc_id must be set, and similar for METABOLITE and hmdb_accession
-
-class MoleculeAlias(models.Model):
-    molecule = models.ForeignKey(Molecule, on_delete=models.CASCADE)
-    alias = models.CharField(max_length=50)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['molecule', 'alias'],
-                name='unique_molecule_alias',
-            )
-        ]
-
-        indexes = [
-            models.Index(fields=['alias'], name='moleculealias_alias_idx')
-        ]
-
-        ordering = ['alias']
-
-        verbose_name = 'molecule alias'
-        verbose_name_plural = 'molecule aliases'
-
-    def __str__(self):
-        return self.alias
