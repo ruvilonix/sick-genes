18a2250d22c8b9df1fbfef14da7c84c8b3f231a6
ruvilonix <a@a.a>
2025-07-01 14:29:51 -0400

    refactored hgnc importer and tests

--- sickgenes/management/commands/import_molecule_data.py
+++ sickgenes/management/commands/import_molecule_data.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from sickgenes.models import Molecule, MoleculeAlias, HgncGene
 import pandas as pd
 from django.utils import timezone
@@ -28,51 +28,100 @@ HMDB_XML_NAME = 'hmdb_metabolites.xml'
 
 @transaction.atomic
 def update_hgnc_data(hgnc_data_path):
-    fields = [
-        'symbol',
-        'name'
-        'entrez_id',
-        'ensembl_gene_id',
-        'vega_id',
-        'ucsc_id',
-        'ena',
-        'uniprot_ids',
-        'pubmed_id',
-        'omim_id',
-        'alias_symbol',
-        'alias_name',
-        'prev_symbol',
-        'prev_name',
-    ]
+    """
+    Updates HgncGene records from JSON data source.
+    
+    Args:
+        hgnc_data_path: Path or URL to HGNC JSON data
+        
+    Returns:
+        int: Number of records processed
+    """
 
+    expected_fields = ['symbol',
+        'name', 'entrez_id', 'ensembl_gene_id', 'vega_id', 'ucsc_id',
+        'ena', 'uniprot_ids', 'pubmed_id', 'omim_id', 'alias_symbol',
+        'alias_name', 'prev_symbol', 'prev_name']
+
+    try:
+        hgnc_data = get_json_from_source(hgnc_data_path)
+        hgnc_genes = hgnc_data['response']['docs']
+    except (KeyError, TypeError) as e:
+        raise CommandError(f'Invalid JSON structure in HGNC data: {e}')
+    except Exception as e:
+        raise CommandError(f'Failed to retrieve HGNC data: {e}')
+    
     datetime_updated = timezone.now()
-
-    hgnc_json_genes = get_json_from_source(hgnc_data_path)['response']['docs']
+    processed_count = 0
     
-    for item in hgnc_json_genes:
-        field_values = {field: item[field] for field in fields if field in item}
-        updated_values = field_values | {'datetime_updated': datetime_updated}
+    for gene_data in hgnc_genes:
+        try:
+            field_values = {
+                field: gene_data[field] 
+                for field in expected_fields 
+                if field in gene_data
+            }
 
-        obj, _ = HgncGene.objects.update_or_create(
-            hgnc_id=item['hgnc_id'],
-            defaults=updated_values,    
-        )
+            field_values['datetime_updated'] = datetime_updated
+
+            obj, _ = HgncGene.objects.update_or_create(
+                hgnc_id=gene_data['hgnc_id'],
+                defaults=field_values,    
+            )
+
+            processed_count += 1
+
+        except KeyError as e:
+            raise CommandError(f"Missing required field in gene data: {e}")
+        except Exception as e:
+            raise CommandError(f"Error processing gene {gene_data.get('hgnc_id', 'unknown')}: {e}")
     
+    return processed_count
 
 class Command(BaseCommand):
-    help = 'Updates Molecule and MoleculeAlias tables with HGNC data. Saves downloaded file to server to allow restoring old versions.'
+    help = 'Imports molecular data from specified database'
 
     def add_arguments(self, parser):
-        parser.add_argument('database', type=str, help="Which database to import: 'hgnc' or 'hmdb'")
-        parser.add_argument('-t', '--test', action='store_true', help="Import data from small test files. Used for testing.")
+        parser.add_argument(
+            'database', 
+            type=str, 
+            choices=['hgnc'],
+            help="Which database to import from"
+        )
+        
+        parser.add_argument(
+            '-t', '--test', 
+            action='store_true', 
+            help="Import data from small test files. Used for testing."
+           )
 
     def handle(self, *args, **kwargs):
-
-        if kwargs['test']:
-            hgnc_data_path = os.path.join(BASE_DIR, 'sickgenes/approved_data/sample_data/sample_hgnc.json')
-        else:
-            hgnc_data_path = HGNC_DATA_PATH
+        database_type = kwargs['database']
+        use_test_data = kwargs['test']
         
         if kwargs['database'] == 'hgnc':
-            update_hgnc_data(hgnc_data_path)
-            self.stdout.write(self.style.SUCCESS('HGNC data successfully imported from JSON'))
+
+            if database_type == 'hgnc':
+                if use_test_data:
+                    data_path = os.path.join(
+                        BASE_DIR,
+                        'sickgenes/approved_data/sample_data/sample_hgnc.json',
+                    )
+                    if not os.path.exists(data_path):
+                        raise CommandError(f"Test data file not found: {data_path}")
+                else:
+                    data_path = HGNC_DATA_PATH
+
+                try:
+                    processed_count = update_hgnc_data(data_path)
+
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'HGNC data successfully imported. '
+                            f'Processed {processed_count} records.'
+                        )
+                    )
+                except CommandError:
+                    raise
+                except Exception as e:
+                    raise CommandError(f'Unexpected error during HGNC import: {e}')
--- sickgenes/tests.py
+++ sickgenes/tests.py
@@ -1,45 +1,58 @@
 from django.test import TestCase, SimpleTestCase
 from django.core.management import call_command
-from sickgenes.models import Molecule, MoleculeAlias, Finding
+from sickgenes.models import HgncGene
 from io import StringIO
 from django.utils import timezone
 from django.urls import reverse
 import datetime
 
-'''
 class ImportHgncTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.out = StringIO()
+        cls.start_time = timezone.now()
         call_command('import_molecule_data', 'hgnc', test=True, stdout=cls.out)
+        cls.end_time = timezone.now()
 
     def test_import_hgnc_success_message(self):
         self.assertIn('HGNC data successfully imported', self.out.getvalue())
 
     def test_hgnc_importer_imports_all_items_from_sample_data(self):
-        molecules = Molecule.objects.all()
-        self.assertEqual(len(molecules), 3)
-
-    def test_hgnc_import_connects_moleculealias_to_molecule(self):
-        molecule = Molecule.objects.get(hgnc_symbol='A1CF')
-        molecule_from_alias = Molecule.objects.get(moleculealias__alias='ACF')
-        self.assertEqual(molecule, molecule_from_alias)
+        genes = HgncGene.objects.all()
+        self.assertEqual(genes.count(), 3)
 
     def test_all_hgnc_fields_are_saved(self):
-        molecule = Molecule.objects.get(hgnc_id='HGNC:5')
+        gene = HgncGene.objects.get(hgnc_id='HGNC:5')
 
-        self.assertEqual(molecule.hgnc_symbol, 'A1BG')
-        self.assertEqual(molecule.hgnc_name, 'alpha-1-B glycoprotein')
-        self.assertEqual(molecule.type, Molecule.MoleculeType.GENE)
+        self.assertEqual(gene.symbol, 'A1BG')
+        self.assertEqual(gene.name, 'alpha-1-B glycoprotein')
+        self.assertEqual(gene.entrez_id, "1")
+        self.assertEqual(gene.ensembl_gene_id, "ENSG00000121410")
+        self.assertEqual(gene.vega_id, "OTTHUMG00000183507")
+        self.assertEqual(gene.ucsc_id, "uc002qsd.5")
+        self.assertEqual(gene.ena, ['fake_ena'])
+        self.assertEqual(gene.uniprot_ids, ["P04217"])
+        self.assertEqual(gene.pubmed_id, [2591067])
+        self.assertEqual(gene.omim_id, [138670])
+        self.assertEqual(gene.alias_symbol, ['fake_alias_symbol'])
+        self.assertEqual(gene.alias_name, ['fake1', 'fake2'])
+        self.assertEqual(gene.prev_name, ['fake_prev_name'])
+        self.assertEqual(gene.prev_symbol, ['fake_prev_symbol'])
 
-    def test_datetime_field_within_past_hour(self):
-        molecule = Molecule.objects.get(hgnc_id='HGNC:5')
+    def test_datetime_field_is_recent(self):
+        gene = HgncGene.objects.get(hgnc_id='HGNC:5')
 
-        self.assertGreaterEqual(molecule.datetime_updated, timezone.now() - datetime.timedelta(hours=1))
-        self.assertLessEqual(molecule.datetime_updated, timezone.now())
+        self.assertGreaterEqual(gene.datetime_updated, self.start_time)
+        self.assertLessEqual(gene.datetime_updated, self.end_time)
 
+    def test_update_or_create_behavior(self):
+        initial_count = HgncGene.objects.count()
 
-'''
+        out = StringIO()
+        call_command('import_molecule_data', 'hgnc', test=True, stdout=out)
+
+        self.assertEqual(HgncGene.objects.count(), initial_count)
+
 class AddMoleculeViewTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get('/manage/add_molecules/')
@@ -50,146 +63,3 @@ class AddMoleculeViewTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'sickgenes/molecule_match.html')
         self.assertContains(response, "Search terms")
-
-class FindMatchingMoleculesTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.molecules = [
-            Molecule.objects.create(
-                hgnc_id='HGNC:1',
-                hgnc_symbol="G1",
-                hgnc_name="gene one",
-                type=Molecule.MoleculeType.GENE,
-            ),
-            Molecule.objects.create(
-                hgnc_id='HGNC:2',
-                hgnc_symbol="G2",
-                hgnc_name="gene two",
-                type=Molecule.MoleculeType.GENE,
-            ),
-            Molecule.objects.create(
-                hmdb_accession='HMDB1',
-                hmdb_name="metabolite one",
-                type=Molecule.MoleculeType.METABOLITE,
-            ),
-            Molecule.objects.create(
-                hgnc_id='HMDB2',
-                hmdb_name="metabolite two",
-                type=Molecule.MoleculeType.METABOLITE,
-            ),
-        ]
-
-        molecule_with_aliases = [
-            (cls.molecules[0], ['G1A1', 'G1A2']),
-            (cls.molecules[1], ['G1A1', 'G1A3']),
-            (cls.molecules[2], ['M1A1', 'M1A2']),
-            (cls.molecules[3], ['M2A3', 'G1A2']),
-        ]
-
-        for molecule in molecule_with_aliases:
-            molecule_aliases = [MoleculeAlias(molecule=molecule[0], alias=alias) for alias in molecule[1]]
-            MoleculeAlias.objects.bulk_create(molecule_aliases)
-
-    def search_all_molecule_types(self, search_strings, 
-            molecule_types=[
-                Molecule.MoleculeType.GENE, 
-                Molecule.MoleculeType.METABOLITE,
-            ]):
-        return Molecule.objects.find_matching_molecules(
-            search_strings, 
-            molecule_types=molecule_types,
-        )  
-    
-    def test_search_results_structure(self):
-        pass
-        #finish
-
-    def test_search_none(self):
-        search_results = self.search_all_molecule_types([])
-        self.assertEqual(len(search_results['no_matches']), 0)
-    
-    def test_search_one_string_with_no_matches(self):
-        search_strings = ['NOTHING']
-        
-        search_results = self.search_all_molecule_types(search_strings)
-
-        self.assertEqual(len(search_results['no_matches']), 1)
-        self.assertEqual(search_results['no_matches'][0], 'NOTHING')
-
-    def test_search_one_string_with_one_molecule_match(self):
-        search_strings = ['HGNC:1']
-
-        search_results = self.search_all_molecule_types(search_strings)
-
-        self.assertEqual(len(search_results['one_match']), 1)
-        self.assertEqual(len(search_results['no_matches']), 0)
-        self.assertEqual(len(search_results['multiple_matches']), 0)
-        self.assertEqual(search_results['one_match'][0]['search_string'], 'HGNC:1')
-        self.assertEqual(search_results['one_match'][0]['molecule'], self.molecules[0])
-
-    
-    def test_search_one_string_with_one_alias_match(self):
-        search_strings = ['M2A3']
-
-        search_results = self.search_all_molecule_types(search_strings)
-
-        self.assertEqual(search_results['one_match'][0]['molecule'], self.molecules[3])
-    
-    def test_search_one_string_with_alias_match_to_two_molecules(self):
-        search_strings = ['G1A1']
-        expected_ids = {'HGNC:1', 'HGNC:2'}
-
-        search_results = self.search_all_molecule_types(search_strings)
-        returned_ids = {mol.hgnc_id for mol in search_results['multiple_matches'][0]['molecules']}
-
-        self.assertEqual(expected_ids, returned_ids)
-
-    def test_search_two_strings_with_same_match(self):
-        search_strings = ['M1A1', 'M1A2']
-        expected_accessions = ['HMDB1', 'HMDB1']
-
-        search_results = self.search_all_molecule_types(search_strings)
-        returned_accessions = [result['molecule'].hmdb_accession for result in search_results['one_match']]
-
-        self.assertEqual(expected_accessions, returned_accessions)
-
-    def test_search_based_on_hgnc_symbol(self):
-        search_results = self.search_all_molecule_types(['G1'])
-        self.assertEqual(search_results['one_match'][0]['molecule'].hgnc_name, 'gene one')
-
-    
-    def test_search_based_on_hgnc_name(self):
-        search_results = self.search_all_molecule_types(['gene one'])
-        self.assertEqual(search_results['one_match'][0]['molecule'].hgnc_symbol, 'G1')
-
-    def test_search_based_on_hmdb_accession(self):
-        search_results = self.search_all_molecule_types(['HMDB1'])
-        self.assertEqual(search_results['one_match'][0]['molecule'].hmdb_name, 'metabolite one')
-
-    def test_search_based_on_hmdb_name(self):
-        search_results = self.search_all_molecule_types(['metabolite one'])
-        self.assertEqual(search_results['one_match'][0]['molecule'].hmdb_accession, 'HMDB1')
-
-    def test_search_various_fields_with_inconsistent_capitalization(self):
-        search_results = self.search_all_molecule_types(['Hgnc:1', 'g1', 'GENE one', 'hmdb1', 'metabolite ONE', 'g1a2'])
-        num_results_with_multiple_matches = len(search_results['multiple_matches'])
-        num_results_with_one_matches = len(search_results['one_match'])
-        
-        self.assertEqual(num_results_with_multiple_matches, 1)
-        self.assertEqual(num_results_with_one_matches, 5)
-
-    def test_search_with_wrong_type(self):
-        search_results = self.search_all_molecule_types(['G1'], molecule_types=[Molecule.MoleculeType.METABOLITE])
-        self.assertEqual(search_results['no_matches'][0], 'G1')
-
-    def test_search_with_correct_type(self):
-        search_results = self.search_all_molecule_types(['G1'], molecule_types=[Molecule.MoleculeType.GENE])
-        self.assertEqual(search_results['one_match'][0]['molecule'].hgnc_symbol, 'G1')
-
-    def test_search_with_no_types(self):
-        pass
-    #TODO finish
-    
-
-class MoleculeModelTests(TestCase):
-    pass
