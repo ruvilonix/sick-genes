66b3f38a1e7afb39d9e61057e459dbec4ade95dd
ruvilonix <a@a.a>
2025-07-08 13:21:43 -0400

    updated tests to work with new model structure for molecules

--- sickgenes/tests.py
+++ sickgenes/tests.py
@@ -121,26 +121,57 @@ class ImportHmdbTest(TestCase):
         self.assertEqual(HmdbMetabolite.objects.count(), initial_count)
 
 
+from django.test import TestCase
+from .models import (
+    HgncGene, Ena, UniprotId, OmimId, AliasSymbol, AliasName, PrevSymbol, PrevName,
+    HmdbMetabolite, MetaboliteSynonym, SecondaryAccession
+)
+
 class FindMatchingHgncGenesTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        # Gene 1 with various associated fields
         cls.gene1 = HgncGene.objects.create(
             hgnc_id='HGNC:1',
             symbol="G1",
             name="gene one",
             entrez_id="101",
-            omim_id=[123, 456],
-            uniprot_ids=['P123', 'P456'],
-            alias_symbol=['G1-ALIAS']
+            ensembl_gene_id="ENSG0000001",
+            vega_id="VEGA:1",
+            ucsc_id="ucsc1",
         )
+        Ena.objects.create(gene=cls.gene1, value="ENA123")
+        UniprotId.objects.create(gene=cls.gene1, value="P123")
+        UniprotId.objects.create(gene=cls.gene1, value="P456")
+        OmimId.objects.create(gene=cls.gene1, value=123)
+        OmimId.objects.create(gene=cls.gene1, value=456)
+        AliasSymbol.objects.create(gene=cls.gene1, value="G1-ALIAS")
+        AliasName.objects.create(gene=cls.gene1, value="One Gene Alias")
+        PrevSymbol.objects.create(gene=cls.gene1, value="G1_PREV")
+        PrevName.objects.create(gene=cls.gene1, value="Previous Gene One Name")
+
+        # Gene 2 with some shared and unique fields
         cls.gene2 = HgncGene.objects.create(
             hgnc_id='HGNC:2',
             symbol="G2",
             name="gene two",
             entrez_id="102",
-            omim_id=[123, 789],
-            uniprot_ids=['item, comma'],
+            ensembl_gene_id="ENSG0000002",
+            vega_id="VEGA:2",
+            ucsc_id="ucsc2",
         )
+        OmimId.objects.create(gene=cls.gene2, value=123) # Shared OMIM ID
+        OmimId.objects.create(gene=cls.gene2, value=789)
+        UniprotId.objects.create(gene=cls.gene2, value='item, comma') # Test for comma in search string
+
+        # Gene 3 for specific field tests
+        cls.gene3 = HgncGene.objects.create(
+            hgnc_id='HGNC:3',
+            symbol="G3",
+            name="gene three",
+            entrez_id="103",
+        )
+
 
     def search_genes(self, search_strings):
         return HgncGene.objects.find_matching_items(search_strings)
@@ -148,40 +179,104 @@ class FindMatchingHgncGenesTests(TestCase):
     def test_search_no_matches(self):
         results = self.search_genes(['NOTHING'])
         self.assertIn('NOTHING', results['no_matches'])
+        self.assertEqual(len(results['one_match']), 0)
+        self.assertEqual(len(results['multiple_matches']), 0)
 
-    def test_search_one_match_by_id(self):
+    # Test individual plain string fields
+    def test_search_by_hgnc_id(self):
         results = self.search_genes(['HGNC:1'])
         self.assertEqual(len(results['one_match']), 1)
         self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['hgnc:1'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
 
-    def test_search_one_match_case_insensitive(self):
-        results = self.search_genes(['hgnc:1'])
+    def test_search_by_symbol(self):
+        results = self.search_genes(['G1'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['g1'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_name(self):
+        results = self.search_genes(['gene one'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['Gene One'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_entrez_id(self):
+        results = self.search_genes(['101'])
         self.assertEqual(results['one_match'][0]['item'], self.gene1)
 
-    def test_search_one_match_by_array_field(self):
+    def test_search_by_ensembl_gene_id(self):
+        results = self.search_genes(['ENSG0000001'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['ensg0000001'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_vega_id(self):
+        results = self.search_genes(['VEGA:1'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['vega:1'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_ucsc_id(self):
+        results = self.search_genes(['ucsc1'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['UCSC1'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    # Test associated string fields
+    def test_search_by_ena_value(self):
+        results = self.search_genes(['ENA123'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['ena123'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_uniprot_id_value(self):
         results = self.search_genes(['P123'])
         self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['p123'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
 
-    def test_search_one_match_by_array_field_case_insensitive(self):
-        results = self.search_genes(['p123'])
+    def test_search_by_alias_symbol_value(self):
+        results = self.search_genes(['G1-ALIAS'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['g1-alias'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_alias_name_value(self):
+        results = self.search_genes(['One Gene Alias'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['one gene alias'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_prev_symbol_value(self):
+        results = self.search_genes(['G1_PREV'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['g1_prev'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    def test_search_by_prev_name_value(self):
+        results = self.search_genes(['Previous Gene One Name'])
+        self.assertEqual(results['one_match'][0]['item'], self.gene1)
+        results_case_insensitive = self.search_genes(['previous gene one name'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.gene1)
+
+    # Test associated integer fields
+    def test_search_by_omim_id_value(self):
+        results = self.search_genes(['456'])
         self.assertEqual(results['one_match'][0]['item'], self.gene1)
 
-    def test_search_multiple_matches_by_omim(self):
+    # Test multiple matches
+    def test_search_multiple_matches_by_shared_omim(self):
         results = self.search_genes(['123'])
         self.assertEqual(len(results['multiple_matches']), 1)
         matched_items = results['multiple_matches'][0]['items']
-        self.assertIn(self.gene1, matched_items)
-        self.assertIn(self.gene2, matched_items)
+        self.assertCountEqual(matched_items, [self.gene1, self.gene2]) # Use assertCountEqual for order-independent check
 
-    def test_search_item_with_comma(self):
+    # Test special characters in search string
+    def test_search_item_with_comma_in_uniprot_id(self):
         results = self.search_genes(['item, comma'])
         self.assertEqual(results['one_match'][0]['item'], self.gene2)
-
-    def test_complex_search(self):
-        results = self.search_genes(['G1', '123', 'nonexistent'])
-        self.assertIn('nonexistent', results['no_matches'])
-        self.assertEqual(results['one_match'][0]['item'], self.gene1)
-        self.assertEqual(len(results['multiple_matches'][0]['items']), 2)
 
 
 class FindMatchingHmdbMetabolitesTests(TestCase):
@@ -190,15 +285,43 @@ class FindMatchingHmdbMetabolitesTests(TestCase):
         cls.metabolite1 = HmdbMetabolite.objects.create(
             accession="HMDB0000001",
             name="1-Methylhistidine",
+            cas_registry_number="123-45-6",
+            drugbank_id="DB001",
+            foodb_id="FDB001",
+            knapsack_id="KNAP001",
+            biocyc_id="BIOCYC001",
+            wikipedia_id="Wikipedia_Metabolite_1",
+            iupac_name="IUPAC Name One",
+            traditional_iupac="Traditional IUPAC One",
+            bigg_id=1,
+            pubchem_compound_id=101,
+            chemspider_id=1001,
             chebi_id=50212,
-            synonyms=["(2S)-2-amino-3-(1-methyl-1H-imidazol-4-yl)propanoic acid", "Pi-methylhistidine"]
         )
+        MetaboliteSynonym.objects.create(metabolite=cls.metabolite1, value="(2S)-2-amino-3-(1-methyl-1H-imidazol-4-yl)propanoic acid")
+        MetaboliteSynonym.objects.create(metabolite=cls.metabolite1, value="Pi-methylhistidine")
+        SecondaryAccession.objects.create(metabolite=cls.metabolite1, value="SECACC001")
+
         cls.metabolite2 = HmdbMetabolite.objects.create(
             accession="HMDB0000002",
             name="2-Methylhistidine",
+            cas_registry_number="789-01-2",
+            drugbank_id="DB002",
+            foodb_id="FDB002",
+            knapsack_id="KNAP002",
+            biocyc_id="BIOCYC002",
+            wikipedia_id="Wikipedia_Metabolite_2",
+            iupac_name="IUPAC Name Two",
+            traditional_iupac="Traditional IUPAC Two",
+            bigg_id=2,
+            pubchem_compound_id=102,
+            chemspider_id=1002,
             chebi_id=50212, # Shared Chebi ID
-            synonyms=["(2S)-2-amino-3-(2-methyl-1H-imidazol-4-yl)propanoic acid", "Tau-methylhistidine"]
         )
+        MetaboliteSynonym.objects.create(metabolite=cls.metabolite2, value="(2S)-2-amino-3-(2-methyl-1H-imidazol-4-yl)propanoic acid")
+        MetaboliteSynonym.objects.create(metabolite=cls.metabolite2, value="Tau-methylhistidine")
+        SecondaryAccession.objects.create(metabolite=cls.metabolite2, value="SECACC002")
+
 
     def search_metabolites(self, search_strings):
         return HmdbMetabolite.objects.find_matching_items(search_strings)
@@ -206,32 +329,116 @@ class FindMatchingHmdbMetabolitesTests(TestCase):
     def test_search_no_matches(self):
         results = self.search_metabolites(['NOT_FOUND'])
         self.assertIn('NOT_FOUND', results['no_matches'])
+        self.assertEqual(len(results['one_match']), 0)
+        self.assertEqual(len(results['multiple_matches']), 0)
 
-    def test_search_one_match_by_accession(self):
+    # Test individual plain string fields
+    def test_search_by_accession(self):
         results = self.search_metabolites(['HMDB0000001'])
         self.assertEqual(len(results['one_match']), 1)
         self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['hmdb0000001'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
 
-    def test_search_one_match_by_name_case_insensitive(self):
-        results = self.search_metabolites(['1-methylhistidine'])
+    def test_search_by_name(self):
+        results = self.search_metabolites(['1-Methylhistidine'])
         self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['1-methylhistidine'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
 
-    def test_search_one_match_by_synonym(self):
+    def test_search_by_cas_registry_number(self):
+        results = self.search_metabolites(['123-45-6'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['123-45-6'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_drugbank_id(self):
+        results = self.search_metabolites(['DB001'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['db001'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_foodb_id(self):
+        results = self.search_metabolites(['FDB001'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['fdb001'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_knapsack_id(self):
+        results = self.search_metabolites(['KNAP001'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['knap001'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_biocyc_id(self):
+        results = self.search_metabolites(['BIOCYC001'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['biocyc001'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_wikipedia_id(self):
+        results = self.search_metabolites(['Wikipedia_Metabolite_1'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['wikipedia_metabolite_1'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_iupac_name(self):
+        results = self.search_metabolites(['IUPAC Name One'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['iupac name one'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_traditional_iupac(self):
+        results = self.search_metabolites(['Traditional IUPAC One'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['traditional iupac one'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    # Test associated string fields
+    def test_search_by_synonym_value(self):
         results = self.search_metabolites(['Pi-methylhistidine'])
         self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['pi-methylhistidine'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
 
-    def test_search_multiple_matches_by_chebi_id(self):
+    def test_search_by_secondary_accession_value(self):
+        results = self.search_metabolites(['SECACC001'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+        results_case_insensitive = self.search_metabolites(['secacc001'])
+        self.assertEqual(results_case_insensitive['one_match'][0]['item'], self.metabolite1)
+
+    # Test integer fields
+    def test_search_by_bigg_id(self):
+        results = self.search_metabolites(['1'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_pubchem_compound_id(self):
+        results = self.search_metabolites(['101'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_chemspider_id(self):
+        results = self.search_metabolites(['1001'])
+        self.assertEqual(results['one_match'][0]['item'], self.metabolite1)
+
+    def test_search_by_chebi_id(self):
         results = self.search_metabolites(['50212'])
         self.assertEqual(len(results['multiple_matches']), 1)
         matched_items = results['multiple_matches'][0]['items']
-        self.assertIn(self.metabolite1, matched_items)
-        self.assertIn(self.metabolite2, matched_items)
+        self.assertCountEqual(matched_items, [self.metabolite1, self.metabolite2])
 
-    def test_combined_search_scenarios(self):
-        results = self.search_metabolites(['HMDB0000002', '50212', 'invalid'])
-        self.assertIn('invalid', results['no_matches'])
+    def test_complex_search_hmdb(self):
+        results = self.search_metabolites(['HMDB0000002', '50212', 'invalid_search_term'])
+        self.assertIn('invalid_search_term', results['no_matches'])
+        
+        # Check one_match
+        self.assertEqual(len(results['one_match']), 1)
+        self.assertEqual(results['one_match'][0]['search_string'], 'HMDB0000002')
         self.assertEqual(results['one_match'][0]['item'], self.metabolite2)
-        self.assertEqual(len(results['multiple_matches'][0]['items']), 2)
+        
+        # Check multiple_matches
+        self.assertEqual(len(results['multiple_matches']), 1)
+        self.assertEqual(results['multiple_matches'][0]['search_string'], '50212')
+        self.assertCountEqual(results['multiple_matches'][0]['items'], [self.metabolite1, self.metabolite2])
 
 class AddGenesView(TestCase):
     @classmethod
