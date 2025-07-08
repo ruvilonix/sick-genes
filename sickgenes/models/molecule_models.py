f37fde910002f75b70a0b6b83b7d02699a951a23
ruvilonix <a@a.a>
2025-07-08 15:33:55 -0400

    removed unnecessary imports

--- sickgenes/models/molecule_models.py
+++ sickgenes/models/molecule_models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
-from django.contrib.postgres.indexes import GinIndex
 from .managers import HgncGeneManager, HmdbMetaboliteManager
 
 
