6d39c6c659d3009660739d6b42d883813227ce0f
ruvilonix <a@a.a>
2025-07-09 12:39:34 -0400

    added date picker input to add study form

--- sickgenes/forms/forms.py
+++ sickgenes/forms/forms.py
@@ -4,7 +4,11 @@ from sickgenes.models import Study, StudyCohort
 class StudyForm(forms.ModelForm):
     class Meta:
         model = Study
-        fields = ['title', 'doi', 'publisher_url', 's4me_url', 'preprint']
+        fields = ['title', 'doi', 'authors', 'publication_date', 'publisher_url', 's4me_url', 'preprint']
+        widgets = {
+            'publication_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
 
 class StudyCohortForm(forms.ModelForm):
     class Meta:
