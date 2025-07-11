f2dae4917385ed0d1390a4a7202c15c08ab2eb74
ruvilonix <a@a.a>
2025-07-10 20:42:03 -0400

    removed staticfiles_dirs setting

--- projectfinding/settings.py
+++ projectfinding/settings.py
@@ -119,9 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
