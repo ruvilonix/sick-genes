f6ed8eb3f729657cbfe7d16c87dbe3ab197a1227
ruvilonix <a@a.a>
2025-07-14 10:44:08 -0400

    added stringinteraction model

--- sickgenes/models/string_models.py
+++ sickgenes/models/string_models.py
@@ -8,3 +8,17 @@ class StringProtein(models.Model):
 
     def __str__(self):
         return self.protein_id
+    
+class StringInteraction(models.Model):
+    protein1 = models.ForeignKey(StringProtein, on_delete=models.CASCADE, related_name='protein1_interactions')
+    protein2 = models.ForeignKey(StringProtein, on_delete=models.CASCADE, related_name='protein2_interactions')
+    combined_score = models.SmallIntegerField()
+
+    def __str__(self):
+        return f'{str(self.protein1)} - {str(self.protein2)}: {self.combined_score}'
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['protein1'], name='stringinteraction_protein1_idx'),
+            models.Index(fields=['protein2'], name='stringinteraction_protein2_idx'),
+        ]
