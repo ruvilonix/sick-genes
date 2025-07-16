4358dad56e6c3bc926e6ccbde2dc530ad452d2c6
ruvilonix <a@a.a>
2025-07-16 15:23:49 -0400

    remove hgnc_id field and cange column name of gene list table

--- sickgenes/tables.py
+++ sickgenes/tables.py
@@ -2,10 +2,10 @@ import django_tables2 as tables
 from .models import HgncGene
 
 class GeneTable(tables.Table):
-    study_count = tables.Column(verbose_name="Distinct Studies")
+    study_count = tables.Column(verbose_name="# of Studies")
 
     class Meta:
         model = HgncGene
         template_name = "django_tables2/bootstrap5.html" 
-        fields = ("symbol", "name", "hgnc_id", "study_count")
+        fields = ("symbol", "name", "study_count")
         order_by = ("-study_count", "symbol")
