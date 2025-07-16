644cbb422c95dfbba807e3598d59c8e03a951534
ruvilonix <a@a.a>
2025-07-16 19:24:14 -0400

    reorganized urls

--- sickgenes/urls.py
+++ sickgenes/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     path('study/<int:study_id>/', views.study, name="study"),
     path('genes/', views.gene_list, name='gene_list'),
     path('gene/<str:hgnc_symbol>/', views.gene_detail, name='gene_detail'),
+    path('search/identify/<str:model_type>/', views.identify_molecules, name='identify_molecules'),
 
     path('manage/add_study/', views.add_study, name="add_study"),
     path('fetch_paper_info/', views.fetch_paper_info, name='fetch_paper_info'),
 
     path('manage/add_study_cohort/<int:study_id>/', views.add_study_cohort, name="add_study_cohort"),
 
-    path('search/identify/<str:model_type>/', views.identify_molecules, name='identify_molecules'),
     path('manage/<int:study_cohort_id>/<str:model_type>/insert/', views.insert_findings, name='insert_findings'),
 
     path('graph/retrieve-network/', views.gene_network_data, name="gene_network_data"),
