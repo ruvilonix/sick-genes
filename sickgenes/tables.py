0ff278e4ac52683b67dba441586e489c5688fe74
ruvilonix <a@a.a>
2025-07-17 10:08:42 -0400

    add studytable

--- sickgenes/tables.py
+++ sickgenes/tables.py
@@ -1,5 +1,5 @@
 import django_tables2 as tables
-from .models import HgncGene
+from .models import HgncGene, Study
 
 class GeneTable(tables.Table):
     study_count = tables.Column(verbose_name="# of Studies")
@@ -8,4 +8,13 @@ class GeneTable(tables.Table):
     class Meta:
         model = HgncGene
         fields = ("symbol", "name", "study_count")
+        order_by = ("-study_count", "symbol")
+
+class StudyTable(tables.Table):
+    gene_count = tables.Column(verbose_name="# of Genes")
+    title = tables.Column(linkify=True)
+
+    class Meta:
+        model = Study
+        fields = ("title", "authors", "gene_count", "created_at")
         order_by = ("-study_count", "symbol")
