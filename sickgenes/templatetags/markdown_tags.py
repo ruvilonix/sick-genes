7ebb058cfd34ac09c2fe78e5c6fccd8c65981dfd
ruvilonix <a@a.a>
2025-07-20 12:24:26 -0400

    removed unneeded markdown template tag

--- sickgenes/templatetags/markdown_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-import markdown
-from django.utils.safestring import mark_safe
-
-register = template.Library()
-
-@register.filter(name='markdown')
-def markdown_format(text):
-    return mark_safe(markdown.markdown(text))
