098895397d0c96fb8383bd6ccfe38b7966c587bc
ruvilonix <a@a.a>
2025-07-25 18:55:40 -0400

    fix gene_list test

--- sickgenes/tests/tests.py
+++ sickgenes/tests/tests.py
@@ -85,8 +85,8 @@ class GeneListTests(TestCase):
         response = self.client.get(reverse('sickgenes:gene_list'))
         genes_in_context = {gene.symbol: gene for gene in response.context['genes_table'].data}
 
-        # Check that all genes are present
-        self.assertEqual(len(genes_in_context), 4)
+        # Check that all genes from finished studies are present
+        self.assertEqual(len(genes_in_context), 3)
 
         # Verify the study count for each gene
         self.assertEqual(genes_in_context['TTN'].study_count, 2)    # Associated with study1 and study3
